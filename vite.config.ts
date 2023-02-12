a9dabe398a810fca011274d13697efb0f41018c2
admin <zhaoqian.ipp@bytedance.net>
2023-02-13 01:11:44 +0800

    fix: add log

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vitest/config";
 const commitHash = require('child_process')
   .execSync('git rev-parse --short HEAD')
-  .toString() ?? process.env.VITE_VERCEL_GIT_COMMIT_SHA;
+  .toString() ?? process.env.VITE_VERCEL_GIT_COMMIT_SHA ?? process.env.VERCEL_GIT_COMMIT_SHA;
+
+console.log('commitHash:', commitHash);
 
 export default defineConfig({
   test: {
